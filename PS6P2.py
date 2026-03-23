7d823cbe618f14d42b39b0f6b233c5dc59fa53e0
Bucky2008 <kk43743@mail.harpercollege.edu>
2026-03-22 23:23:19 -0500

    Add files via upload

---
+++ PS6P2.py
@@ -0,0 +1,13 @@
+#input phase 
+start = int(input("Enter the start value:"))
+stop = int(input("Enter the stop value:"))
+increment = int(input("Enter the increment value:"))
+
+#process phase 
+current = start
+
+while current <= stop:
+   print(current)
+   current = current + increment
+
+                
